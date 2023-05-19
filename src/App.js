9bf85c2b9a9ac0d2218ed2bbfa0a145c7cc4e36f
Sangamesh math <sangameshmath99@gmail.com>
2023-05-19 15:45:11 +0530

    use of maps to print objects of a list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // const name = <h1>Hello, Sangamesh</h1>;
+  const name = [{nam: "sangamesh" ,age: 18},{nam: "sagar" ,age: 21}, {nam: "suraj" ,age: 23},];
+  return (<div>
+    {name.map((user, key) => {
+      return <User name = {user.nam} age = {user.age}/>
+    
+})}
     </div>
   );
+  
 }
+const User = (props) =>
+  {
+    return (
+    <div>
+      <h2 style={{color: "red"}}>{props.name} {props.age}</h2>
+    </div> );
+  };
+
 
 export default App;
